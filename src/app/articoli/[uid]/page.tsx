83679e34b2c8edee11a54e6fab47f31e7fd1ef41
grafica <paolo@monema.it>
2024-10-24 11:20:41 +0200

    video embed

--- src/app/articoli/[uid]/page.tsx
+++ src/app/articoli/[uid]/page.tsx
@@ -34,7 +34,7 @@ export default async function Page({ params }: { params: Params }) {
       );
     },
   };
-
+  console.log(page);
   return (
     <main className={style.blogPage}>
       {page.data.mainimage.url && (
